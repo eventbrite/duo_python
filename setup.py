c0cd9cb1476de464dbec3b17218813f51d6a84dd
Ashwini Oruganti <ashwini@eventbrite.com>
2017-02-23 13:34:26 -0800

    Add eventbrite vendor versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='duo_python',
-    version='0.1.0',
+    version='0.1.0+eventbrite',
     author='DuoSecurity',
     author_email='duo@duosecurity.com',
     packages=['duo_web',],
